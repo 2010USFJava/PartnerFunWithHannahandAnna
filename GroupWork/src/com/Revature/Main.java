ef155bc79467e95d7e67bdc5aa0db1e9f0f830ff
Hannah Brown <2hbrown13@gmail.com>
2020-10-21 15:37:26 -0500

    Toy Bean

--- GroupWork/src/com/Revature/Main.java
+++ GroupWork/src/com/Revature/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Toy a= new Toy("Pig","Piggie",4);
+		System.out.println(a);
+		Toy b =new Toy("Rope","rope",7);
+		System.out.println(b);
+		Toy c =new Toy("Ball","ball",0);
+		System.out.println(c);
+				
 	}
 
 }
