7af7cab7fc1ce6cea7b4556474b44bbc785d6b0d
Hannah Brown <68199458+hannahbrown97@users.noreply.github.com>
2020-10-21 16:47:31 -0500

    Update Main.java

--- GroupWork/src/com/Revature/Main.java
+++ GroupWork/src/com/Revature/Main.java
@@ -4,23 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 
 		System.out.println("He has many toys.");
-=======
-		System.out.println("This is the main method");
-		
->>>>>>> 2a4ad0eb634fba75a1c776b8613a44d75e08a057
+
 		Toy a= new Toy("Pig","Piggie",4);
 		System.out.println(a);
 		Toy b =new Toy("Rope","rope",7);
 		System.out.println(b);
 		Toy c =new Toy("Ball","ball",0);
 		System.out.println(c);
-<<<<<<< HEAD
+
 		System.out.println("Meet my puppy!");
-=======
->>>>>>> 2a4ad0eb634fba75a1c776b8613a44d75e08a057
+
 		Dog r= new Dog("Romeo",55,"Border Collie",a);
 		System.out.println(r);
 	}
